15e4e8dbb2de338290b4a52fd8511ca6aabe30c2
BMSrusky <brunomiguelsilvajogos@hotmail.com>
2022-04-24 17:12:01 +0100

    modificacoes viewmodels
    alteracao de xaml e xaml.cs

---
+++ spump/spump/MVM/View/SalasView.xaml.cs
@@ -0,0 +1,111 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace spump.MVM.View
+{
+    /// <summary>
+    /// Interação lógica para SalasView.xam
+    /// </summary>
+    public partial class SalasView : UserControl
+    {
+        public SalasView()
+        {
+            InitializeComponent();
+            CarregaDadosGrid();
+        }
+
+        private void CarregaDadosGrid()
+        {
+            BD b = new BD();
+            b.LigaBD();
+            try
+            {
+                DataTable dt = new DataTable();
+                string query = string.Format("SELECT * FROM salas;");
+                MySqlDataAdapter da = new MySqlDataAdapter(query, b.con);
+                da.Fill(dt);
+                grid.ItemsSource = dt.DefaultView;// Área para fazer algo mais com a grid view...
+            }
+            catch
+            {
+                MessageBox.Show("Não foram retornados dados!", "ERRO");
+            }
+            b.FechaBD();
+        }
+
+        private void butInserir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void butEditar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BD b = new BD();
+            b.LigaBD();
+            try
+            {
+                DataTable dt = new DataTable();
+                string query = string.Format("SELECT * FROM salas where nome like '" + search.Text + "%' or NumeroSala like '" + search.Text + "%';");
+                MySqlDataAdapter da = new MySqlDataAdapter(query, b.con);
+                da.Fill(dt);
+                grid.ItemsSource = dt.DefaultView;// Área para fazer algo mais com a grid view...
+            }
+            catch
+            {
+                MessageBox.Show("Não foram retornados dados!", "ERRO");
+            }
+            b.FechaBD();
+        }
+        private void grid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName == "dataNascimento")// Atenção ao campo da datagrid! Neste exemplo chama-se dataF.
+            {
+                DataGridTextColumn coluna = e.Column as DataGridTextColumn;
+                Binding binding = coluna.Binding as Binding;
+                binding.StringFormat = "yyyy-MM-dd";
+
+            }
+        }
+
+        private void grid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            butInserir.IsEnabled = true;
+            butEditar.IsEnabled = true;
+            try
+            {
+                //if (gridProfessores.SelectedItem != null)
+                //{
+                //    DataRowView v = (DataRowView)gridProfessores.SelectedItem;
+                //    codP = int.Parse(v[0].ToString());
+                //    nome = v[1].ToString();
+                //    dataN = v[2].ToString();
+                //    salario = double.Parse(v[4].ToString());
+                //    contacto = v[3].ToString();
+                //}
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
--- spump/spump/MVM/ViewModel/MainViewModel.cs
+++ spump/spump/MVM/ViewModel/MainViewModel.cs
@@ -11,9 +11,21 @@ namespace spump.MVM.ViewModel
     {
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand ClientesViewCommand { get; set; }
+        public RelayCommand ProfessoresViewCommand { get; set; }
+        public RelayCommand AulasGrupoViewCommand { get; set; }
+        public RelayCommand ExerciciosViewCommand { get; set; }
+        public RelayCommand ModalidadesViewCommand { get; set; }
+        public RelayCommand PlanoTreinoViewCommand { get; set; }
+        public RelayCommand SalasViewCommand { get; set; }
 
         public HomeViewModel HomeVM { get; set; }
         public ClientesViewModel ClientesVM { get; set; }
+        public ProfessoresViewModel ProfessoresVM { get; set; }
+        public AulasGrupoViewModel AulasGrupoVM { get; set; }
+        public ExerciciosViewModel ExerciciosVM { get; set; }
+        public ModalidadesViewModel ModalidadesVM { get; set; }
+        public PlanoTreinoViewModel PlanoTreinoVM { get; set; }
+        public SalasViewModel SalasVM { get; set; }
 
         private object _currentView;
 
@@ -29,6 +41,12 @@ namespace spump.MVM.ViewModel
         {
             HomeVM = new HomeViewModel();
             ClientesVM = new ClientesViewModel();
+            ProfessoresVM = new ProfessoresViewModel();
+            AulasGrupoVM = new AulasGrupoViewModel();
+            ExerciciosVM = new ExerciciosViewModel();
+            ModalidadesVM = new ModalidadesViewModel();
+            PlanoTreinoVM = new PlanoTreinoViewModel();
+            SalasVM = new SalasViewModel();
             CurrentView = HomeVM;
 
             HomeViewCommand = new RelayCommand(o =>
@@ -39,6 +57,36 @@ namespace spump.MVM.ViewModel
             ClientesViewCommand = new RelayCommand(o =>
             {
                 CurrentView = ClientesVM;
+            });
+
+            ProfessoresViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ProfessoresVM;
+            });
+
+            AulasGrupoViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = AulasGrupoVM;
+            });
+
+            ExerciciosViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ExerciciosVM;
+            });
+
+            PlanoTreinoViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PlanoTreinoVM;
+            });
+
+            ModalidadesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ModalidadesVM;
+            });
+
+            SalasViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SalasVM;
             });
         }
     }
